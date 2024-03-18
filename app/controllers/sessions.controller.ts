d279dc6e47540e792702bb7981738f39facd31a2
JinJin_Hrb <fanwang1987@gmail.com>
2024-03-18 20:39:26 +0800

    feat: batch rename file names

--- app/controllers/sessions.controller.ts
+++ app/controllers/sessions.controller.ts
@@ -16,6 +16,9 @@ import { validateOnServer } from '../lib/formily.json-schema.umd.staging'
 import schema20221124 from '../mock/schema20221124'
 import data20221124 from '../mock/data20221124'
 import dayjs from 'dayjs'
+import { listFilteredFilesPromise } from 'app/helpers/fsUtils'
+import fs from 'fs'
+import path from 'path'
 
 @JsonController()
 @Service()
@@ -48,5 +51,66 @@ export class SessionsController {
     const result = await validateOnServer(schema20221124, data20221124)
     console.log('#41 result:', result)
     return { message: 'OK', result }
+  }
+
+  @Post('/rename')
+  async rename(@Body() params: any) {
+    const { folderPath } = params
+    const candidates = (await listFilteredFilesPromise({
+      filterHandler: a => (a.fname !== '.DS_Store' ? a.fileFlag ?? false : false),
+      folderPath,
+      isRecur: false,
+      mapHandler: a => {
+        //   {
+        //     "dev": 16777220,
+        //     "mode": 33188,
+        //     "nlink": 1,
+        //     "uid": 501,
+        //     "gid": 20,
+        //     "rdev": 0,
+        //     "blksize": 4096,
+        //     "ino": 181028743,
+        //     "size": 8196,
+        //     "blocks": 24,
+        //     "atimeMs": 1710750095827.3193,
+        //     "mtimeMs": 1710743304000,
+        //     "ctimeMs": 1710749980485.7163,
+        //     "birthtimeMs": 1710743304000,
+        //     "atime": "2024-03-18T08:21:35.827Z",
+        //     "mtime": "2024-03-18T06:28:24.000Z",
+        //     "ctime": "2024-03-18T08:19:40.486Z",
+        //     "birthtime": "2024-03-18T06:28:24.000Z",
+        //     "life": 20681007,
+        //     "lifeInDay": 0,
+        //     "fname": ".DS_Store",
+        //     "fileFlag": true,
+        //     "directoryFlag": false,
+        //     "symbolicLinkFlag": false,
+        //     "absPath": "/Users/alexwang/Downloads/xTransfer/静态资源/一系列小图片/.DS_Store",
+        //     "depth": 0
+        // }
+        return a.fname as string
+      },
+    })) as string[]
+
+    const promises = candidates.map(a =>
+      a.includes('&') || a.includes('/') || a.includes('\\') || /\.{2,}|\s/.test(a)
+        ? new Promise((r, j) => {
+            const oldPath = path.join(folderPath, a)
+            const b = encodeURIComponent(a)
+              .replace(/\.{2,}/g, '.')
+              .replace(/\s/g, '')
+            const newPath = path.join(folderPath, b)
+            fs.rename(oldPath, newPath, function (err) {
+              if (err) console.log('ERROR: ' + err)
+            })
+            r(b)
+          })
+        : a,
+    )
+
+    const result = await Promise.all(promises)
+
+    return { result }
   }
 }
